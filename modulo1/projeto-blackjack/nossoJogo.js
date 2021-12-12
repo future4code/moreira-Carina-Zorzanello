f987bc447617398abb0d72b07abe7e127bea36a6
ninazorzanello <carina.zorzanello@gmail.com>
2021-12-12 14:37:10 -0300

    projeto-blackjack

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -15,14 +15,10 @@
 
     if(confirm("Quer iniciar uma nova rodada?")) {
      
-      rodada()
+      rodada()  
       
    } else {
          console.log("O jogo acabou")
-   }
-
-   function cartaSorteada(carta) {
-
    }
 
 
@@ -30,37 +26,51 @@
       const usuario = []
       const computador = []
       let texto
-      let valor
+      let valorUsuario
+      let valorComputador
 
-     usuario[0] = comprarCarta()
-     texto = usuario.map(retornaTexto);
-     valor = usuario.map(retornaValor)
-
-     usuario[0] = comprarCarta()
-     texto = texto + " " + usuario.map(retornaTexto)
-     valor = Number(valor) + Number(usuario.map(retornaValor))
-
-
-
-     usuario[0] = comprarCarta()
-     texto = computador.map(retornaTexto);
-     valor = computador.map(retornaValor)
-
-     usuario[0] = comprarCarta()
-     texto = texto + " " + computador.map(retornaTexto)
-     valor = Number(valor) + Number(computador.map(retornaValor))
-
-     console.log(`Usuário - cartas: ${texto} - pontuação ${valor}`)
-     console.log(`Computador - cartas: ${texto} - pontuação ${valor}`)
-  
-  
       function retornaTexto(item){
          return item.texto
       }
       function retornaValor(item){
          return Number(item.valor)
       }
-    
+
+     usuario[0] = comprarCarta()
+     texto = usuario.map(retornaTexto);
+     valorUsuario = usuario.map(retornaValor)
+
+     usuario[0] = comprarCarta()
+     texto = texto + " " + usuario.map(retornaTexto)
+     valorUsuario = Number(valorUsuario) + Number(usuario.map(retornaValor))
+
+     console.log(`Usuário - cartas: ${texto} - pontuação ${valorUsuario}`)
+
+
+     computador[0] = comprarCarta()
+     texto = computador.map(retornaTexto);
+     valorComputador = computador.map(retornaValor)
+
+     computador[0] = comprarCarta()
+     texto = texto + " " + computador.map(retornaTexto)
+     valorComputador = Number(valorComputador) + Number(computador.map(retornaValor))
+
+     
+     console.log(`Computador - cartas: ${texto} - pontuação ${valorComputador}`)
+  
+     if (valorUsuario > valorComputador){
+        console.log("O usuário ganhou!")
+     } else if (valorComputador > valorUsuario){
+        console.log("O computador ganhou!")
+     } else {
+        console.log("Empate!")
+     }
    }
+
+  
+
+
+
+
   
    
