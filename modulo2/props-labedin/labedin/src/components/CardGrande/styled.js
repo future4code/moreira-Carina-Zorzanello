9cef8b17bacc571e18185b21771e2acdb218f8de
ninazorzanello <carina.zorzanello@gmail.com>
2022-01-11 16:14:02 -0300

    props-labedin

---
+++ modulo2/props-labedin/labedin/src/components/CardGrande/styled.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const ContainerPrincipal = styled.div`
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+    padding: 20px 10px;
+    margin-bottom: 10px;
+    height: 200px;
+`;
+
+export const Imagem = styled.img`
+    width: 70px;
+    margin-right: 10px;
+    border-radius: 50%;
+`;
+
+export const Nome = styled.h4`
+     margin-bottom: 15px;
+`;
+
+export const DivContainer = styled.div`
+     display: flex;
+    flex-direction: column;
+    justify-items: flex-start;
+`;
+
