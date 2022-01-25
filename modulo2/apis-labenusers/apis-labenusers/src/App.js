bb98a6020fe52bac1a5b59797aa1d6a01f456743
ninazorzanello <carina.zorzanello@gmail.com>
2022-01-25 16:58:34 -0300

    criacao de usuarios utilizando apis

---
+++ modulo2/apis-labenusers/apis-labenusers/src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import CriarUsuario from "./components/CriarUsuario";
+import ListaUsuarios from "./components/ListaUsuarios";
+
+  export default class App extends React.Component {
+
+    state = {
+      pagina: "criar",
+    };
+
+    irParaTela = () => {
+      if(this.state.pagina === "criar"){
+        this.setState({pagina: "listar"})
+      }else{
+        this.setState({ pagina: "criar" });
+      }
+     
+    };
+    render(){
+
+    
+      const renderizaTelaCorreta = () => {
+        switch (this.state.pagina) {
+          case "criar":
+            return <CriarUsuario />;
+          case "listar":
+            return <ListaUsuarios />;
+          default:
+            return <div> Erro! Página não encontrada</div>;
+        }
+      };
+
+    return( 
+      <div>
+      <button onClick={this.irParaTela}>Trocar de Tela</button>
+      <div className="App">{renderizaTelaCorreta()}</div>
+      </div>
+    )
+  
+      
+        
+    }
+  
+  }
+
+
