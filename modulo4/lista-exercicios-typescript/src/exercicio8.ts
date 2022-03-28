9f05f59df7192d61450174e08da87f0d32da8083
ninazorzanello <carina.zorzanello@gmail.com>
2022-03-28 13:22:37 -0300

    exercicios

--- modulo4/lista-exercicios-typescript/src/exercicio8.ts
+++ modulo4/lista-exercicios-typescript/src/exercicio8.ts
@@ -1,19 +1,19 @@
-function checaRenovacaoRG(){
+// function checaRenovacaoRG(){
  
-    const anoAtual: Number = Number(prompt("Digite o ano atual"))
-    const anoNascimento: Number = Number(prompt("Digite o seu ano de nascimento"))
-    const emissaoRg: Number = Number(prompt("Digite o ano de emissao do seu rg"))
-    const idade: Number = anoAtual - anoNascimento
-    const anosId: Number = anoAtual - emissaoRg
+//     const anoAtual: Number = Number(prompt("Digite o ano atual"))
+//     const anoNascimento: Number = Number(prompt("Digite o seu ano de nascimento"))
+//     const emissaoRg: Number = Number(prompt("Digite o ano de emissao do seu rg"))
+//     const idade: Number = anoAtual - anoNascimento
+//     const anosId: Number = anoAtual - emissaoRg
 
-    const dataAtual: Date= new Date()
+//     const dataAtual: Date= new Date()
 
-    const resultado1 = idade <= 20 && anosId == 5 
-    const resultado2 = idade > 20 && idade <= 50 && anosId >= 10
-    const resultado3 = idade > 50 && anosId >= 15
+//     const resultado1 = idade <= 20 && anosId == 5 
+//     const resultado2 = idade > 20 && idade <= 50 && anosId >= 10
+//     const resultado3 = idade > 50 && anosId >= 15
     
-    console.log(resultado1 || resultado2 || resultado3)
+//     console.log(resultado1 || resultado2 || resultado3)
    
-}
+// }
 
-checaRenovacaoRG()
+// checaRenovacaoRG()
