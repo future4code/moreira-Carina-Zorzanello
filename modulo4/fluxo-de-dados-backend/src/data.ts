ea286fb49ef62bd2d11c15cbcb315b753f0f603c
ninazorzanello <carina.zorzanello@gmail.com>
2022-03-30 16:51:52 -0300

    exercicios

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,36 @@
+export type Itens = {
+    id: number,
+    name: string,
+    price: number
+ }
+ 
+ export const produtos: Itens[] =
+  [
+      { 
+         "id": 1,
+         "name":"Sapato",
+         "price": 12 
+      
+     },
+     { 
+        "id": 2,
+        "name":"Bolsa",
+        "price": 20 
+     
+    },
+    { 
+        "id": 3,
+        "name":"Cabide",
+        "price": 30 
+     
+    },
+    { 
+        "id": 4,
+        "name":"Chapeu",
+        "price": 15 
+     
+    },
+   
+    
+ 
+ ]
