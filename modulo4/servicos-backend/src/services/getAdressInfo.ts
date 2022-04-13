2f2c7024f883faa74501053d1d9fba52e025a2e2
ninazorzanello <carina.zorzanello@gmail.com>
2022-04-13 15:37:53 -0300

    exercicio quarta

---
+++ modulo4/servicos-backend/src/services/getAdressInfo.ts
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import { Address } from '../types/Adress'
+
+//Exercício 1
+
+//https://viacep.com.br/ws/:cep/json/
+
+const baseUrl = "https://viacep.com.br/ws"
+
+export const getAddressInfo = async (zipcode: string): Promise<Address | null> => {
+    try {
+        const response = await axios.get(`${baseUrl}/${zipcode}/json/`)
+        const address: Address = {
+            state: response.data.uf,
+            city: response.data.localidade,
+            district: response.data.bairro,
+            street: response.data.logradouro
+        }
+
+        return address
+    } catch (error) {
+        console.error("Erro no serviço getAddressInfo: ", error)
+        return null
+    }
+}
+
+
+//Exercício 2
+
